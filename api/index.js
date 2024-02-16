43ba8f60adc16a18742596056853bb8133855810
Kaizen <alimcdad0@gmail.com>
2024-02-16 15:58:19 +0200

    commit

---
+++ api/index.js
@@ -0,0 +1,56 @@
+var Express = require("express");
+var Mongoclient = require("mongodb").MongoClient;
+var cors = require("cors");
+var multer = require("multer");
+
+var app = Express();
+app.use(cors());
+
+var CONNECTION_STRING = "mongodb+srv://admin:admin@cluster0.v4kccoe.mongodb.net/?retryWrites=true&w=majority"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var DATABASENAME = "todoappdb";
+var database;
+
+app.listen(5038, () => {
+    Mongoclient.connect(CONNECTION_STRING, (error, client) => {
+        database = client.db(DATABASENAME);
+        console.log("Mongo DB connected succssefully!")
+    });
+})
+
+app.get('/api/todoapp/getnotes', (request, response) => {
+    database.collection("todoappcollection").find({}).toArray((error, result) => {
+        response.send(result);
+    })
+})
+
+app.post('/api/todoapp/addnotes', multer().none(), (request, response) => {
+    database.collection("todoappcollection").count({}, function (error, numOfDocs) {
+        database.collection("todoappcollection").insertOne({
+            id: (numOfDocs + 1).toString(),
+            description: request.body.newNotes
+        });
+        response.json("Added successfully!")
+    })
+})
+
+app.delete('/api/todoapp/deletenotes', (request, response) => {
+    database.collection("todoappcollection").deleteOne({
+        id:request.query.id
+    })
+    response.json("Deleted successfully!")
+})
